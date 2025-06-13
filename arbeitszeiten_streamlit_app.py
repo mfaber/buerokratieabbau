a6e852416d91bb7657a5627efc76c775fb664a96
mfaber <mfaber@users.noreply.github.com>
2025-06-13 13:35:21 +0200

    Add files via upload

--- arbeitszeiten_streamlit_app.py
+++ arbeitszeiten_streamlit_app.py
@@ -53,8 +53,8 @@ def extract_times_from_pdf(pdf_bytes):
 
     df = pd.DataFrame(extrahiert, columns=["Datum", "Wochentag", "Von1", "Bis1", "Von2", "Bis2"])
 
-    # 🧹 Erste Zeile entfernen, wenn Tag < 01
-    df = df[df["Datum"].str.match(r"^(0[1-9]|[12][0-9]|3[01])\..")]
+    # ❗ Filter: Nur Zeilen mit Datum 01.–31. zulassen
+    df = df[df["Datum"].str.match(r"^(0[1-9]|[12][0-9]|3[01])\.")]
 
     def parse_time(text):
         match = re.match(r"(\d{1,2})[:\.]?(\d{2})", str(text))
