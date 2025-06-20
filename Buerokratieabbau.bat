72c058c4923a7c30fa9b78be2f135007b6d3a92a
mfaber <mfaber@users.noreply.github.com>
2025-06-20 16:55:44 +0200

    Add files via upload

---
+++ Buerokratieabbau.bat
@@ -0,0 +1,6 @@
+Bitte Python aus dem RKI-Softwarekiosk installieren, sonst funktioniert das hier nicht ;)
+pause
+pip install streamlit pandas pdfplumber openpyxl
+streamlit run arbeitszeiten_app.py
+
+
